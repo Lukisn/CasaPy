ceed71e34d5bbb3134e56a3f92d691f113627ecd
Lukas Reimer <lukas.reimer@icloud.com>
2022-12-12 11:14:38 +0100

    Added more templates, view implementations and routing (tutorial part 3)

--- CasaWeb/casaweb/polls/views.py
+++ CasaWeb/casaweb/polls/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
+    context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
 
-# TODO: continue here: https://docs.djangoproject.com/en/4.1/intro/tutorial03/#raising-a-404-error
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
