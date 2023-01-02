a79c3f60f21cc93dd3ee9d9c14c94df590b0aed7
Lukas Reimer <lukas.reimer@icloud.com>
2023-01-02 20:42:22 +0100

    created signup and login views without backend implementation

--- CasaWeb/casaweb/home/urls.py
+++ CasaWeb/casaweb/home/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "home"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
 ]
--- CasaWeb/casaweb/home/views.py
+++ CasaWeb/casaweb/home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/index.html")
+
+
+def login(request):
+    return render(request, "home/login.html")
+
+
+def signup(request):
+    return render(request, "home/signup.html")
