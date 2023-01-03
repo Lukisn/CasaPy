99ef27a4c74e42eb2d0ac7f150ce5e43876f7281
Lukas Reimer <lukas.reimer@icloud.com>
2023-01-03 22:08:44 +0100

    implemented signup logic

--- CasaWeb/casaweb/home/views.py
+++ CasaWeb/casaweb/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def home_view(request):
@@ -13,10 +14,10 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Redirect to a success page.
             return redirect("home:index")
         else:
-            # Return an 'invalid login' error message.
+            # TODO: flash message
+            # https://docs.djangoproject.com/en/4.1/ref/contrib/messages/
             print("INVALID LOGIN!!!")
     return render(request, "home/login.html")
 
@@ -27,4 +28,17 @@ def logout_view(request):
 
 
 def signup_view(request):
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+        if password1 == password2:
+            user = User.objects.create_user(username, password=password1)
+            print(user)
+            login(request, user)
+            return redirect("home:index")
+        else:
+            # TODO: flash message
+            print("PASSWORDS MUST MATCH!")
     return render(request, "home/signup.html")
