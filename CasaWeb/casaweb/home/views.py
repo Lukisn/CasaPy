57cc038500ea2216b2d199439958ca4bc17f912b
Lukas Reimer <lukas.reimer@icloud.com>
2023-01-03 22:35:38 +0100

    added success and error messages using django messages framework

--- CasaWeb/casaweb/home/views.py
+++ CasaWeb/casaweb/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def home_view(request):
@@ -14,31 +15,36 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.add_message(request, messages.SUCCESS, "Login successful")
             return redirect("home:index")
         else:
-            # TODO: flash message
-            # https://docs.djangoproject.com/en/4.1/ref/contrib/messages/
-            print("INVALID LOGIN!!!")
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "Username or Password incorrect",
+                extra_tags="danger",
+            )
     return render(request, "home/login.html")
 
 
 def logout_view(request):
     logout(request)
+    messages.add_message(request, messages.SUCCESS, "Logout successful")
     return redirect("home:index")
 
 
 def signup_view(request):
     if request.method == "POST":
-        print(request.POST)
         username = request.POST["username"]
         password1 = request.POST["password1"]
         password2 = request.POST["password2"]
         if password1 == password2:
             user = User.objects.create_user(username, password=password1)
-            print(user)
             login(request, user)
+            messages.add_message(request, messages.SUCCESS, "Signup successful")
             return redirect("home:index")
         else:
-            # TODO: flash message
-            print("PASSWORDS MUST MATCH!")
+            messages.add_message(
+                request, messages.ERROR, "Passwords must match", extra_tags="danger"
+            )
     return render(request, "home/signup.html")
