afe07eb3e7358b579c9efbf7f6a6707c0bd58728
Lukas Reimer <lukas.reimer@icloud.com>
2023-01-06 15:50:08 +0100

    styled contact and issue forms

--- CasaWeb/casaweb/home/forms.py
+++ CasaWeb/casaweb/home/forms.py
@@ -6,10 +6,19 @@ class ContactForm(Form):
     email = EmailField(label="Email", required=True)
     message = CharField(label="Message", widget=Textarea, required=True)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self:
+            field.field.widget.attrs["class"] = "input"
+
 
 class IssueForm(Form):
     name = CharField(label="Your Name", max_length=200, required=True)
     email = EmailField(label="Email", required=True)
     subject = CharField(label="Subject", max_length=200, required=True)
     report = CharField(label="Issue Report", widget=Textarea, required=True)
-    # TODO: add file upload for screenshots etc.
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "input"
